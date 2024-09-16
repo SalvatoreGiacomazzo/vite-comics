f656b8ec43d7ea1678807f3dc4743f04e252d59d
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-16 22:55:02 +0200

    imported components

---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<template>
+
+    <img src="../assets/img/dc-logo.png" alt="comic logo">
+
+</template>
+
+<style></style>
