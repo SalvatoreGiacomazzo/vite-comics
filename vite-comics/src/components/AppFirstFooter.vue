cc821b665851d9cbc66d7211b198b066e6bc0512
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-17 01:12:07 +0200

    exercise finished with bonus (header and part of the first footer to be fixed)

--- vite-comics/src/components/AppFirstFooter.vue
+++ vite-comics/src/components/AppFirstFooter.vue
@@ -11,6 +11,52 @@ export default {
 
 <template>
     <footer id="first-footer">
+        <div id="ff-info">
+            <h2>DC COMICS</h2>
+            <ul>
+                <li><a href="#"></a>Characters</li>
+                <li><a href="#"></a>Comics</li>
+                <li><a href="#"></a>Movies</li>
+                <li><a href="#"></a>TV</li>
+                <li><a href="#"></a>Games</li>
+                <li><a href="#"></a>Videos</li>
+                <li><a href="#"></a>News</li>
+            </ul>
+
+
+            <h2> DC</h2>
+            <ul>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy Policy (New)</a></li>
+                <li><a href="#"></a>Ad Choices</li>
+                <li><a href="#"></a>Advertising</li>
+                <li><a href="#"></a>Jobs</li>
+                <li><a href="#"></a>Subscriptions</li>
+                <li><a href="#"></a>Talent Workshops</li>
+                <li><a href="#"></a>CPSC Certificates</li>
+                <li><a href="#"></a>Rating</li>
+                <li><a href="#"></a>Shop Help</li>
+                <li><a href="#"></a>Contact Us</li>
+            </ul>
+
+
+            <h2>SITES</h2>
+            <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC KIDS</a></li>
+                <li><a href="#">DC UNIVERSE</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+
+            </ul>
+            <h2>SHOP</h2>
+            <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+        </div>
+
+        <img src="../assets/img/dc-logo-bg.png" alt="">
 
     </footer>
 
