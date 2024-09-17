7ba6ee748ad78cad51dcd6c8b9b64befba091566
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-17 17:29:56 +0200

    added header link dynamically

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -2,7 +2,69 @@
 export default {
     data() {
         return {
-
+        navBarLinks : [
+            {
+                id: '1',
+                label: 'Characters',
+                url:'#',
+                active: false
+            },
+                {
+                id: '2',
+                label: 'Comics',
+                url:'#',
+                active: true
+            },
+            {
+                id: '3',
+                label: 'Movies',
+                url:'#',
+                active: false
+            },
+            {
+                id: '4',
+                label: 'TV',
+                url:'#',
+                active: false
+            },
+            {
+                id: '5',
+                label: 'Games',
+                url:'#',
+                active: false
+            },
+            {
+                id: '6',
+                label: 'Collectibles',
+                url:'#',
+                active: false
+            },
+            {
+                id: '7',
+                label: 'Videos',
+                url:'#',
+                active: false
+            },
+            {
+                id: '8',
+                label: 'Fans',
+                url:'#',
+                active: false
+            },
+            {
+                id: '9',
+                label: 'News',
+                url:'#',
+                active: false
+            },
+            {
+                id: '10',
+                label: 'Shop',
+                url:'#',
+                active: false
+            },
+            
+        ]
         }
     }
 }
@@ -17,20 +79,21 @@ export default {
         <div class="navbar">
             <nav>
                 <ul>
-                    <li><a href="#"></a>Characters</li>
-                    <li><a href="#"></a>Comics</li>
-                    <li><a href="#"></a>Movies</li>
-                    <li><a href="#"></a>TV</li>
-                    <li><a href="#"></a>Games</li>
-                    <li><a href="#"></a>Collectibles</li>
-                    <li><a href="#"></a>Videos</li>
-                    <li><a href="#"></a>Fans</li>
-                    <li><a href="#"></a>News</li>
-                    <li><a href="#"></a>Shop</li>
+                    <li v-for="navBarItem in navBarLinks" :key="navBarItem.id" class="{active : navBarItem.active}">
+                        <a :href="navBarItem.url">
+                            {{ navBarItem.label }}
+                        </a>
+                    </li>
+                    
                 </ul>
             </nav>
         </div>
     </header>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+a{
+    color:rgb(69, 66, 66);
+    text-decoration: none;
+}
+</style>
