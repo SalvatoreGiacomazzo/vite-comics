c4c398b191d58a95848ea7e6454b429ff44f0695
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-09-17 18:16:27 +0200

    social img in footer added dynamically

--- vite-comics/src/components/AppSecondFooter.vue
+++ vite-comics/src/components/AppSecondFooter.vue
@@ -2,7 +2,38 @@
 export default {
     data() {
         return {
-
+          socials: [
+            {
+                id:'1',
+                socialUrl:'footer-facebook.png',
+                alt:'facebook'
+            },
+            {
+                id:'2',
+                socialUrl:'footer-periscope.png',
+                alt:'periscope'
+            },
+            {
+                id:'3',
+                socialUrl:'footer-pinterest.png',
+                alt:'pinterest'
+            },
+            {
+                id:'4',
+                socialUrl:'footer-twitter.png',
+                alt:'twitter'
+            },
+            {
+                id:'5',
+                socialUrl:'footer-youtube.png',
+                alt:'youtube'
+            },
+          ]
+        }
+    },
+    methods:{
+        getSocialImg(socialUrl){
+            return new URL(`../assets/img/${socialUrl}`, import.meta.url).href
         }
     }
 }
@@ -12,15 +43,22 @@ export default {
 <template>
     <footer id="second-footer">
         <button>SIGN UP NOW!</button>
-        <div id="sf-icons">
+        <div id="second-footer-left">
             <h2>FOLLOW US</h2>
-            <img src="../assets/img/footer-facebook.png" alt="">
-            <img src="../assets/img/footer-periscope.png" alt="">
-            <img src="../assets/img/footer-pinterest.png" alt="">
-            <img src="../assets/img/footer-twitter.png" alt="">
-            <img src="../assets/img/footer-youtube.png" alt="">
+        <div id="sf-icons" v-for="(social, id) in socials" :key="social.id" >
+            
+            <img :src="getSocialImg(social.socialUrl)" :alt="social.alt">
+           
+        </div>
         </div>
     </footer>
 </template>
 
-<style></style>
+<style scoped>
+#second-footer-left{
+    display:flex;
+}
+h2{
+    color:dodgerblue;
+}
+</style>
